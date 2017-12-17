42d7b8a13ae1866f83ffdc5df2e1a3d5deaf23f8
reneeb <info@perl-services.de>
2017-12-17 08:43:35 +0100

    show the notes on mouseover

--- Kernel/Output/HTML/FilterContent/ArticleNotesIcons.pm
+++ Kernel/Output/HTML/FilterContent/ArticleNotesIcons.pm
@@ -12,12 +12,11 @@ use strict;
 use warnings;
 
 our @ObjectDependencies = qw(
-    Kernel::Language
     Kernel::Output::HTML::Layout
-    Kernel::System::Web::Request
     Kernel::Config
     Kernel::System::Ticket
-    Kernel::System::Log
+    Kernel::System::JSON
+    Kernel::Language
 );
 
 sub new {
@@ -33,16 +32,19 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
-    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
-    my $JSONObject   = $Kernel::OM->Get('Kernel::System::JSON');
-    my $TicketObject = $Kernel::OM->Get('Kernel::System::Ticket');
-    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject   = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $JSONObject     = $Kernel::OM->Get('Kernel::System::JSON');
+    my $TicketObject   = $Kernel::OM->Get('Kernel::System::Ticket');
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
+    my $LanguageObject = $Kernel::OM->Get('Kernel::Language');
 
     my @ArticleIDs = ${ $Param{Data} } =~ m{<input \s+ type="hidden" \s+ class="ArticleID" \s+ value="([^"]+)"}xmsg;
 
     my %ArticlesWithNotes;
+    my %ArticleNotes;
 
-    my $Name = $ConfigObject->Get('ArticleNotes::Field') || 'ArticleNote';
+    my $Name  = $ConfigObject->Get('ArticleNotes::Field') || 'ArticleNote';
+    my $Title = $LanguageObject->Translate('Note');
 
     for my $ArticleID ( @ArticleIDs ) {
         my %Article = $TicketObject->ArticleGet(
@@ -53,22 +55,72 @@ sub Run {
 
         if ( $Article{"DynamicField_$Name"} ) {
             $ArticlesWithNotes{$ArticleID} = 1;
+            $ArticleNotes{$ArticleID} = $LayoutObject->Output(
+                Template => '[% Data.Text | html %]',
+                Data     => { Text => $Article{"DynamicField_$Name"} },
+            );
         }
     }
 
-    my $JSON = $JSONObject->Encode( Data => \%ArticlesWithNotes );
-    my $Code = qq~
-        var ArticlesWithNotes = $JSON;
-        \$('input[class="ArticleID"]').each( function() {
-            var ArticleID = \$(this).val();
+    my $JSON      = $JSONObject->Encode( Data => \%ArticlesWithNotes );
+    my $NotesJSON = $JSONObject->Encode( Data => \%ArticleNotes );
 
-            if( ArticlesWithNotes[ArticleID] == 1 ) {
-                \$(this).parent().append( '<span class="fa fa-exclamation-circle"></span>' );
+    my $Code = qq~
+        <script type="text/javascript">//<![CDATA[
+        function PSArticleNotesShow( ArticleID, Event ) {
+            var ArticleNotes = $NotesJSON;
+ 
+            var PosX = 0;
+            var PosY = 0;
+
+            var jsEvent = Event || window.event;
+            if (jsEvent.pageX || jsEvent.pageY) {
+
+                PosX = jsEvent.pageX;
+                PosY = jsEvent.pageY;
+             }
+            else if (jsEvent.clientX || jsEvent.clientY) {
+
+                PosX = jsEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                PosY = jsEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
             }
-        });
+
+            // increase X position to don't be overlapped by mouse pointer
+            PosX = PosX + 15;
+
+            var Note = ArticleNotes[ArticleID];
+            var Layer = '<div id="events-layer" class="Hidden" style="position:absolute; top: ' + PosY + 'px; left:' + PosX + 'px; z-index: 999;"> ' +
+                '    <div style="z-index: 5; background-color: #EEE; border: 1px solid #CCC; padding: 6px"><h2>$Title</h2><pre>' +
+                         Note +
+                '    </pre></div> ' +
+                '</div> ';
+
+            \$(Layer).appendTo('body');
+            \$('#events-layer').fadeIn('fast');
+        }
+
+        Core.App.Ready( function() {
+            var ArticlesWithNotes = $JSON;
+            \$('input[class="ArticleID"]').each( function() {
+                var ArticleID = \$(this).val();
+
+                if( ArticlesWithNotes[ArticleID] == 1 ) {
+                    var Span = \$( '<span class="fa fa-exclamation-circle"></span>' );
+                    \$(this).parent().append( Span );
+
+                    Span.bind('mouseover', function( event ) {
+                        PSArticleNotesShow( ArticleID, event );
+                    }).bind( 'mouseout', function() {
+                        \$('#events-layer').fadeOut('fast');
+                        \$('#events-layer').remove();
+                    });
+                }
+            });
+         });
+        //]]></script>
     ~;
 
-    ${ $Param{Data} } =~ s{Core\.App\.Ready.*?\{\K}{$Code};
+    ${ $Param{Data} } =~ s{</body>}{$Code</body>}xms;
 
     return 1;
 }
